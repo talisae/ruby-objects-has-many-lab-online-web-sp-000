ef5a0d43e6ae6121adba680dd3975f629e82d1b8
Talisa Erazo <talisaerazo@gmail.com>
2019-01-02 22:46:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,19 +14,3 @@ class Song
   end
 
 end
-
-class Song
-  attr_accessor :name, :artist
-
-  def initialize(name)
-    @name = name
-  end
-
-  def artist_name
-    if @artist == nil
-      return nil
-    else
-      @artist.name
-    end
-  end
-end
