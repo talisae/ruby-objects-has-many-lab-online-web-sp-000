33d009da1ec19217f588f3a162acc752b65baef3
Talisa Erazo <talisaerazo@gmail.com>
2019-01-02 22:24:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,14 +6,13 @@ class Artist
     @songs = []
   end
 
-  def songs
-    @songs
-  end
-
   def add_song(song)
     @songs << song
     song.artist = self
   end
 
+  def songs
+    @songs
+  end
   
 end
